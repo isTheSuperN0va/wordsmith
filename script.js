9cdefab10dc5f1404beae1e730e43303082135ce
Eduardo Oliveira <ed.wardolivacap@gmail.com>
2025-11-28 12:16:34 -0300

    + botões, + melhor função de fittext, + sistema de seleção

--- script.js
+++ script.js
@@ -4,26 +4,56 @@
 // Add a clear button in the word div
 
 sylabbles = document.querySelectorAll(".sylabbles li");
-word = document.querySelector("#word");
+wordContainer = document.querySelector("#wordContainer");
+word = document.querySelector("#word")
+words = document.querySelector("#wordsLi");
 spanFromWord = document.querySelector("#word span")
 sylArray = [];
 
 if (sylabbles.length === 0) { console.log("li is null") }
 
 
-window.addEventListener("resize", () => {
-    fitText(word);
-})
+// window.addEventListener("resize", () => {
+//     fitText(wordContainer);
+// })
+
+sylArray = ["to", "ki"]
+addToWord(sylArray);
 
 
 sylabbles.forEach(element => {
     element.addEventListener("click", () => {
         console.log("clicked " + element.textContent)
         sylArray.push(element.textContent)
+        
+        console.log(sylArray[0]);
         addToWord(sylArray)
-        fitText(word)
+        
+         document.fonts?.ready
+            .then(() => requestAnimationFrame(() => fitText(wordContainer)))
+            .catch(() => requestAnimationFrame(() => fitText(wordContainer)));
     })
 });
+
+Array.from(word.children).forEach(span => {
+    span.addEventListener("click", () => {
+        console.log("clicked " + span.classList)
+
+        
+        if (span.classList != "selected-sylabble") {
+            deselectAllSyls();
+            span.classList.add("selected-sylabble");
+        }
+        else span.classList.remove("selected-sylabble");
+
+    })
+})
+
+function deselectAllSyls() {
+    Array.from(word.children).forEach(span => {
+        if (span.classList != null) { span.classList.remove("selected-sylabble"); }
+    })
+}
 
 
 function addToWord(syls) {
@@ -34,6 +64,8 @@ function addToWord(syls) {
         let span = document.createElement("span")
         span.textContent = element
         word.appendChild(span)
+
+        console.log("added " + element + " to word")
     })
 }
 
@@ -41,9 +73,9 @@ function addToWord(syls) {
 
 
 function fitText(container) {
-    let children = Array.from(container.children)
+    let children = Array.from(word.children)
 
-    let spansWidth = calculateSpansWidth(container);
+    let spansWidth = calculateSpansWidth(word);
     let boxWidth = calculateBoxWidth(container);
 
     let size = parseFloat(getComputedStyle(children[0]).fontSize);
@@ -57,7 +89,7 @@ function fitText(container) {
             element.style.fontSize = size + "px";
         })
 
-        spansWidth = calculateSpansWidth(container);
+        spansWidth = calculateSpansWidth(word);
         boxWidth = calculateBoxWidth(container);
 
 
@@ -71,18 +103,26 @@ function fitText(container) {
 }
 
 
-function isTooBig(size, boxSize) { return size > (boxSize); }
-function isTooSmall(size, boxSize) { return size < (boxSize - (boxSize*0.2)); }
+function isTooBig(size, boxSize) { return size > (boxSize) - (boxSize*0.2); }
+function isTooSmall(size, boxSize) { return size < (boxSize - (boxSize*0.4)); }
 
 function calculateBoxWidth(container) { return container.clientWidth; }
 
 function calculateSpansWidth(container) {
+
     let children = Array.from(container.children);
+
+    if (children == null) { console.error("no children dectected in the word container"); }
+    else { console.error(children[0].offsetWidth); }
+
+
     let spansWidth = 0;
 
     children.forEach(span => {
-        spansWidth += span.clientWidth
+        spansWidth += span.offsetWidth
     })
+
+    if (spansWidth == 0) { console.log("spanswidth returned 0") }
 
     return spansWidth;
 
@@ -93,6 +133,36 @@ function copyWords() {
 
     console.log(text)
     navigator.clipboard.writeText(text).then(() => {
-        console.log("Copied text")
+        console.log("Copied " + text);
     });
+}
+
+function clearWord() { 
+    word.innerHTML = "";
+    sylArray = [];
+    console.log("cleared word"); 
+}
+
+function clearWords() {
+    Array.from(words.children).forEach(element => {
+        element.remove();
+        console.log("accessing " + element);
+    })
+
+    console.log("removed stored words")
+}
+
+function addWordToList() {
+    let arrangedWord = "";
+
+    sylArray.forEach(syl => {
+        arrangedWord += syl;
+    })
+
+    console.log("arranged to " + arrangedWord);
+
+    li = document.createElement("li");
+    li.textContent = arrangedWord;
+    words.appendChild(li)
+
 }
